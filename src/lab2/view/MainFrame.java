ebf3d5bec1a21e254c2626592f4d4d267c5e3cb5
Oleg Dobrovolski <odobro33@gmail.com>
2019-04-08 00:12:49 +0300

    added menubar, action listeners are now private classes inside the mainframe

--- src/lab2/view/MainFrame.java
+++ src/lab2/view/MainFrame.java
@@ -7,15 +7,19 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 public class MainFrame {
     private JFrame frame = new JFrame();
     Controller controller;
     private StudentsTable studentsTable;
+    private JToolBar bar;
 
     public MainFrame() {
         this.controller = new Controller();
         this.studentsTable = new StudentsTable();
+        assembleToolBar();
     }
 
     public JFrame buildFrame() {
@@ -27,26 +31,17 @@ public class MainFrame {
         addEntry.addActionListener(ev -> new AddEntryDialog(frame, studentsTable, controller).buildDialog().setVisible(true));
         JMenu searchEntries = new JMenu("Search for entries");
         JMenuItem searchByNameGroup = new JMenuItem("Search for entries by name and Group");
-        searchByNameGroup.addActionListener(ev -> new EntryNameGroupSearchDialog(new Controller(controller)).buildDialog().setVisible(true));
+        searchByNameGroup.addActionListener(new searchNameGroupListener());
         JMenuItem searchByNameWork = new JMenuItem("Search for entries by name and social work hours");
-        searchByNameWork.addActionListener(ev -> new EntryNameWorkSearchDialog(new Controller(controller)).buildDialog().setVisible(true));
+        searchByNameWork.addActionListener(new searchNameWorkListener());
         JMenuItem searchByGroupWork = new JMenuItem("Search for entries by group and social work hours");
-        searchByGroupWork.addActionListener(ev -> new EntryGroupWorkSearchDIalog(new Controller(controller)).buildDialog().setVisible(true));
+        searchByGroupWork.addActionListener(new searchGroupWorkListener());
         JMenuItem deleteByNameGroup = new JMenuItem("Delete entries by name and group");
-        deleteByNameGroup.addActionListener(ev -> {
-            new EntryNameGroupDeleteDialog(controller).buildDialog().setVisible(true);
-            studentsTable.setTableData(controller.representData());
-        });
+        deleteByNameGroup.addActionListener(new deleteNameGroupListener());
         JMenuItem deleteByNameWork = new JMenuItem("Delete entries by name and work hours");
-        deleteByNameWork.addActionListener(ev -> {
-            new EntryNameWorkDeleteDialog(controller).buildDialog().setVisible(true);
-            studentsTable.setTableData(controller.representData());
-        });
+        deleteByNameWork.addActionListener(new deleteNameWorkListener());
         JMenuItem deleteByGroupWork = new JMenuItem("Delete entries by group and work hours");
-        deleteByGroupWork.addActionListener(ev -> {
-            new EntryGroupWorkDeleteDialog(controller).buildDialog().setVisible(true);
-            studentsTable.setTableData(controller.representData());
-        });
+        deleteByGroupWork.addActionListener(new deleteGroupWorkListener());
         searchEntries.add(searchByNameGroup);
         searchEntries.add(searchByNameWork);
         searchEntries.add(searchByGroupWork);
@@ -55,30 +50,9 @@ public class MainFrame {
         deleteEntries.add(deleteByNameWork);
         deleteEntries.add(deleteByGroupWork);
         JMenuItem saveToFile = new JMenuItem("Save to file");
-        saveToFile.addActionListener(ev -> {
-            JFileChooser chooser = new JFileChooser();
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("XML files", "xml");
-            chooser.setFileFilter(filter);
-            int chooserVal;
-            chooserVal = chooser.showSaveDialog(frame);
-            if (chooserVal == JFileChooser.APPROVE_OPTION) {
-                File file = chooser.getSelectedFile();
-                controller.saveData(file.getPath());
-            }
-        });
+        saveToFile.addActionListener(new fileSaveListener());
         JMenuItem loadFromFile = new JMenuItem("Load from file");
-        loadFromFile.addActionListener(ev -> {
-            JFileChooser chooser = new JFileChooser();
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("XML files", "xml");
-            chooser.setFileFilter(filter);
-            int chooserVal;
-            chooserVal = chooser.showOpenDialog(frame);
-            if (chooserVal == JFileChooser.APPROVE_OPTION) {
-                File file = chooser.getSelectedFile();
-                controller.loadData(file.getPath());
-                studentsTable.setTableData(controller.representData());
-            }
-        });
+        loadFromFile.addActionListener(new fileLoadListener());
         JMenu operationsMenu = new JMenu("Data manipulation");
         operationsMenu.add(searchEntries);
         operationsMenu.add(deleteEntries);
@@ -94,8 +68,99 @@ public class MainFrame {
         rootPanel.setLayout(new BorderLayout());
         DefaultTableModel studentsTableModel = studentsTable.getTableModel();
         rootPanel.add(studentsTable.createTableComponent(), BorderLayout.CENTER);
+        rootPanel.add(bar, BorderLayout.NORTH);
         rootPanel.validate();
         frame.add(rootPanel);
         return frame;
+    }
+
+    private void assembleToolBar(){
+        bar = new JToolBar();
+        JButton barSearchNameGroup = new JButton("search by name and group");
+        barSearchNameGroup.addActionListener(new searchNameGroupListener());
+        JButton barSearchNameWork = new JButton("search by name and work");
+        barSearchNameWork.addActionListener(new searchNameWorkListener());
+        JButton barSearchGroupWork = new JButton("search by group and work");
+        barSearchGroupWork.addActionListener(new searchGroupWorkListener());
+        JButton barDeleteNameGroup = new JButton("delete by name and group");
+        barDeleteNameGroup.addActionListener(new deleteNameGroupListener());
+        JButton barDeleteNameWork = new JButton("delete by name and work");
+        barDeleteNameWork.addActionListener(new deleteNameWorkListener());
+        JButton barDeleteGroupWork = new JButton("delete by group and work");
+        barDeleteGroupWork.addActionListener(new deleteGroupWorkListener());
+        JButton barFileSave = new JButton("Save to file");
+        barFileSave.addActionListener(new fileSaveListener());
+        JButton barFileLoad = new JButton("Load from file");
+        barFileLoad.addActionListener(new fileLoadListener());
+        bar.add(barSearchNameGroup);
+        bar.add(barSearchNameWork);
+        bar.add(barSearchGroupWork);
+        bar.add(barDeleteNameGroup);
+        bar.add(barDeleteNameWork);
+        bar.add(barDeleteGroupWork);
+        bar.add(barFileSave);
+        bar.add(barFileLoad);
+        bar.setFloatable(false);
+    }
+
+    private class searchNameGroupListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            new EntryNameGroupSearchDialog(new Controller(controller)).buildDialog().setVisible(true);
+        }
+    }
+    private class searchNameWorkListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            new EntryNameWorkSearchDialog(new Controller(controller)).buildDialog().setVisible(true);
+        }
+    }
+    private class searchGroupWorkListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            new EntryGroupWorkSearchDIalog(new Controller(controller)).buildDialog().setVisible(true);
+        }
+    }
+    private class deleteNameGroupListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            new EntryNameGroupDeleteDialog(controller).buildDialog().setVisible(true);
+            studentsTable.setTableData(controller.representData());
+        }
+    }
+    private class deleteNameWorkListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            new EntryNameWorkDeleteDialog(controller).buildDialog().setVisible(true);
+            studentsTable.setTableData(controller.representData());
+        }
+    }
+    private class deleteGroupWorkListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            new EntryGroupWorkDeleteDialog(controller).buildDialog().setVisible(true);
+            studentsTable.setTableData(controller.representData());
+        }
+    }
+    private class fileSaveListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            JFileChooser chooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("XML files", "xml");
+            chooser.setFileFilter(filter);
+            int chooserVal;
+            chooserVal = chooser.showSaveDialog(frame);
+            if (chooserVal == JFileChooser.APPROVE_OPTION) {
+                File file = chooser.getSelectedFile();
+                controller.saveData(file.getPath());
+            }
+        }
+    }
+    private class fileLoadListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            JFileChooser chooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("XML files", "xml");
+            chooser.setFileFilter(filter);
+            int chooserVal;
+            chooserVal = chooser.showOpenDialog(frame);
+            if (chooserVal == JFileChooser.APPROVE_OPTION) {
+                File file = chooser.getSelectedFile();
+                controller.loadData(file.getPath());
+                studentsTable.setTableData(controller.representData());
+            }
+        }
     }
 }
